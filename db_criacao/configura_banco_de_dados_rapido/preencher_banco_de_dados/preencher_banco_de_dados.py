9b25c8cad192ef1679e2136f6f38c90bf6783ef0
vcsil <vcsilva@poil.ufrj.br>
2023-12-06 20:33:29 -0300

    :sparkles: feat: Cria e testa conexão com banco de dados

---
+++ db_criacao/configura_banco_de_dados_rapido/preencher_banco_de_dados/preencher_banco_de_dados.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+"""
+preencher_banco_de_dados.
+
+Script para pegar os dados do Bling e passar para o banco de dados
+"""
+import time
+
+from dotenv import dotenv_values
+from pathlib import Path
+
+import psycopg
+
+# Path consegue lidar com diretório em vários Sistemas Operacionais
+arquivo_env = Path(".") / ".." / ".." / ".." / ".env"
+
+# Pega os valores das variáveis de ambiente
+env = dotenv_values(dotenv_path=arquivo_env)
+
+# Construindo a string de conexão
+conn_string = f"""
+    dbname={env["POSTGRES_DATABASE"]}
+    user={env["POSTGRES_USERNAME"]}
+    password={env["POSTGRES_PASSWORD"]}
+    host={env["POSTGRES_HOST"]}
+    port={env["POSTGRES_PORT"]}
+"""
+
+t0 = time.perf_counter()
+
+try:
+    # Conecte-se a um banco de dados existente
+    with psycopg.connect(conn_string) as conn:
+
+        # Execute a command: this creates a new table
+        conn.execute("""
+            CREATE TABLE test (
+                id serial PRIMARY KEY,
+                num integer,
+                data text)
+            """)
+
+        # Passe os dados para preencher os espaços reservados de uma consulta
+        # e deixe o Psycopg executar a conversão correta (sem injeções de SQL!)
+        conn.execute(
+            "INSERT INTO test (num, data) VALUES (%s, %s)",
+            (100, "abc'def"))
+
+        # Consulte o banco de dados e obtenha dados como objetos Python.
+        conn.execute("SELECT * FROM test").fetchone()
+        # retornará (1, 100, "abc'def")
+
+        # Você pode usar `cur.fetchmany()`, `cur.fetchall()` para retornar uma
+        # lista de vários registros, ou até mesmo iterar no cursor
+        for record in conn.execute("SELECT * FROM test"):
+            print(record)
+
+        # Faça as alterações no banco de dados persistentes
+        # conn.commit()
+
+
+except psycopg.Error as e:
+    print(f"Erro ao conectar ao banco de dados: {e}")
+
+finally:
+    dt = time.perf_counter() - t0
+    print(f'{dt:0.3f}s')
