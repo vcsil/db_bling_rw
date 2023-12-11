73f8d59d9e64f23b78a13825091a4d0d2013da4c
vcsil <vcsilva@poil.ufrj.br>
2023-12-11 17:14:24 -0300

    :sparkles: feat: Cria funções para comunicação com o banco de dados

--- db_criacao/configura_banco_de_dados_rapido/preencher_banco_de_dados/preencher_banco_de_dados.py
+++ db_criacao/configura_banco_de_dados_rapido/preencher_banco_de_dados/preencher_banco_de_dados.py
@@ -165,6 +165,9 @@ def solicita_na_api(ROTA: str, header: Dict[str, Optional[str]]):
 header = cria_header_http(access_token=env['OAUTH_ACCESS_TOKEN'])
 
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-
+
+# =-=-=-=-=-=-=-=-=-=-=-=-= Conexão ao Banco de Dados =-=-=-=-=-=-=-=-=-=-=-=-=
+
 # Construindo a string de conexão
 conn_string = f"""
     dbname={env["POSTGRES_DATABASE"]}
@@ -174,15 +177,64 @@ conn_string = f"""
     port={env["POSTGRES_PORT"]}
 """
 
-t0 = time.perf_counter()
 
-try:
+def select_all_from_db(
+        tabela: str,
+        colunas: Union[str, List[str]],
+        conn_string: str,
+        filtro: Tuple[str, Tuple[Union[str, int]]] = None
+) -> List[Tuple[int]]:
+    """
+    Faz um SELECT no banco de dados.
+
+    É possivel definir a tabela, as colunas e algum tipo de filtro
+
+    Parameters
+    ----------
+    tabela : str
+        Nome da tabela.
+    colunas : Union[str, List[str]]
+        Nome da coluna ou um array com o nome das colunas .
+    conn_string : str
+        String de conexão com banco de dados.
+    filtro : Tuple[str, Tuple[Union[str, int]]], optional
+        Uma tupla com o Filtro seguindo por uma tupla com o valor do filtro.
+        The default is None.
+
+    Returns
+    -------
+    List[Tuple[int]]
+        Uma lista com as linhas em forma de tupla com os dados.
+
+    """
+    if isinstance(colunas, str):
+        colunas = [colunas]
+
+    query = (
+        sql.SQL("SELECT {columns} FROM {table}")
+        .format(
+            columns=sql.SQL(',').join(
+                    map(sql.Identifier, colunas)
+            ),
+            table=sql.SQL(tabela)
+        )
+    )
+    if filtro:
+        query = sql.SQL("{query_fixa} {fltr} {tabela}").format(
+            query_fixa=query,
+            fltr=sql.SQL(filtro[0]),
+            tabela=sql.Literal(filtro[1])
+        )
+
+    try:
     # Conecte-se a um banco de dados existente
-    with psycopg.connect(conn_string) as conn:
+        with psycopg.connect(conn_string) as conn:
+            # print(query.as_string(conn))
+            array_dados = conn.execute(query).fetchall()
 
-        # Execute a command: this creates a new table
-        conn.execute("""
-            CREATE TABLE test (
+        return array_dados
+    except psycopg.Error as e:
+        print(f"Erro ao conectar ao banco de dados: {e}")
                 id serial PRIMARY KEY,
                 num integer,
                 data text)
@@ -194,22 +246,76 @@ try:
             "INSERT INTO test (num, data) VALUES (%s, %s)",
             (100, "abc'def"))
 
-        # Consulte o banco de dados e obtenha dados como objetos Python.
-        conn.execute("SELECT * FROM test").fetchone()
-        # retornará (1, 100, "abc'def")
+def insert_in_db(
+        tabela: str,
+        colunas: Union[str, List[str]],
+        valores: List[Dict[str, Union[str, int]]],
+        valores_placeholder: List[str],
+        conn_string: str
+):
+    """
+    Faz um INSERT no banco de dados.
 
-        # Você pode usar `cur.fetchmany()`, `cur.fetchall()` para retornar uma
-        # lista de vários registros, ou até mesmo iterar no cursor
-        for record in conn.execute("SELECT * FROM test"):
-            print(record)
+    Parameters
+    ----------
+    tabela : str
+        Nome da tabela.
+    colunas : Union[str, List[str]]
+        Nome da coluna.
+    valores : List[Dict[str, Union[str, int]]]
+        Uma lista com os valores a serem inseridos em tupla.
+        Nome da coluna.
+    valores_placeholder : List[str]
+        Lista utilizada para nomear cada coluna que vai receber o valor.
 
-        # Faça as alterações no banco de dados persistentes
-        # conn.commit()
+    Returns
+    -------
+    None.
+
+    """
+    query = (
+        sql.SQL("INSERT INTO {table} ({columns}) VALUES ({values})")
+        .format(
+            table=sql.Identifier(tabela),
+            columns=sql.SQL(',').join(
+                    map(sql.Identifier, colunas)
+            ),
+            values=sql.SQL(', ').join(
+                    map(sql.Placeholder, valores_placeholder)
+            )
+        )
+    )
+
+    try:
+        with psycopg.connect(conn_string) as conn:
+            with conn.cursor() as cur:
+                cur.executemany(query, valores)
+    except psycopg.Error as e:
+        print(f"Erro ao conectar ao banco de dados: {e}")
 
 
-except psycopg.Error as e:
-    print(f"Erro ao conectar ao banco de dados: {e}")
+def pega_nome_colunas(tabela: str) -> List[str]:
+    """
+    Pegar o nome de todas as colunas de uma tabela.
 
-finally:
-    dt = time.perf_counter() - t0
-    print(f'{dt:0.3f}s')
+    Parameters
+    ----------
+    tabela : str
+        Nome da tabela.
+
+    Returns
+    -------
+    array_dados : List[str]
+        Lista de tupla com os nomes das colunas.
+
+    """
+    nome_colunas = select_all_from_db(tabela='"information_schema"."columns"',
+                                      colunas="column_name",
+                                      filtro=("WHERE table_name =", tabela),
+                                      conn_string=conn_string)
+    # Tirar os valores da tupla e deixar dentro de uma array
+    nome_colunas = [coluna[0] for coluna in nome_colunas]
+
+    return nome_colunas
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
