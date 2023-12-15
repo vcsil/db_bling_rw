9bf89430dc36d4ef455e0e6ae66f9cbe9cf759d5
vcsil <vcsilva@poil.ufrj.br>
2023-12-15 19:08:04 -0300

    :wrench: feat: adiciona linha sobre data de inscrição

--- db_criacao/configura_banco_de_dados_rapido/scripts/019-contatos.sql
+++ db_criacao/configura_banco_de_dados_rapido/scripts/019-contatos.sql
@@ -17,6 +17,7 @@ CREATE TABLE "contatos"(
     "data_nascimento"                   DATE,
     "sexo"                              INTEGER             NOT NULL CHECK (sexo IN (1, 2, 3)) DEFAULT 2,
     "id_classificacao_contato"          BIGINT                       REFERENCES "contatos_classificacao"("id_bling"),
+    "cliente_desde"			TIMESTAMPTZ	    NOT NULL DEFAULT current_timestamp
 );
 COMMENT ON COLUMN
     "contatos"."numero_documento" IS 'CPF ou CNPJ do contato';
