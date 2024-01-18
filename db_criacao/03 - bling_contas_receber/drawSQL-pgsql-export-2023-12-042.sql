82735268196873ec0fd549eb47c921617d1ec0b5
vcsil <vcsilva@poil.ufrj.br>
2024-01-18 11:19:20 -0300

    :recycle: refactor: Atualiza estrutura de contas do banco de dados

--- db_criacao/03 - bling_contas_receber/drawSQL-pgsql-export-2023-12-042.sql
+++ db_criacao/03 - bling_contas_receber/drawSQL-pgsql-export-2023-12-042.sql
@@ -2,15 +2,47 @@
 -----------------#   CONTAS RECEBER   #-----------------
 
 CREATE TABLE "contas_contabeis"(
-    "id"        BIGINT PRIMARY KEY  NOT NULL,
-    "descricao" VARCHAR(45)         NOT NULL
+    "id_bling"  BIGINT PRIMARY KEY  NOT NULL,
+    "nome"      VARCHAR(45)         NOT NULL  CHECK ("descricao" <> '')
 );
 
 ---*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
-CREATE TABLE "contas_receber_situacao"(
+CREATE TABLE "categorias_receitas_despesas_tipo"(
+    "id"    BIGINT PRIMARY KEY  NOT NULL,
+    "nome"  VARCHAR(18)         NOT NULL CHECK ("nome" <> '')
+);
+COMMENT ON COLUMN
+    "categorias_receitas_despesas_tipo"."nome" IS '`1` Despesa
+    `2` Receita
+    `3` Receita e despesa';
+
+---*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+CREATE TABLE "categorias_receitas_despesas"(
+    "id_bling"      BIGINT PRIMARY KEY  NOT NULL,
+    "nome"          VARCHAR(45)         NOT NULL CHECK ("nome" <> ''),
+    "id_tipo"       INTEGER          	NOT NULL REFERENCES "categorias_receitas_despesas_tipo"("id"),
+    "situacao"	    BOOLEAN		NOT NULL
+);
+COMMENT ON COLUMN
+    "categorias_receitas_despesas"."tipo" IS '`1` Despesa
+    `2` Receita
+    `3` Receita e despesa';
+
+---*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+CREATE TABLE "categorias_receitas_despesas_relacao"(
+    "id"                    SERIAL PRIMARY KEY  NOT NULL,
+    "id_categoria_pai"      BIGINT              NOT NULL REFERENCES "categorias_receitas_despesas"("id_bling"),
+    "id_categoria_filho"    BIGINT              NOT NULL UNIQUE REFERENCES "categorias_receitas_despesas"("id_bling")
+);
+
+---*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+CREATE TABLE "contas_situacao"(
     "id"    SERIAL PRIMARY KEY  NOT NULL,
-    "nome"  VARCHAR(45)         NOT NULL
+    "nome"  VARCHAR(45)         NOT NULL  CHECK ("nome" <> '')
 );
 COMMENT ON COLUMN
     "contas_receber_situacao"."nome" IS '`1` Em aberto
@@ -23,7 +55,7 @@ COMMENT ON COLUMN
 
 CREATE TABLE "tipos_pagamento"(
     "id"    SERIAL PRIMARY KEY  NOT NULL,
-    "nome"  VARCHAR(63)         NOT NULL
+    "nome"  VARCHAR(63)         NOT NULL  CHECK ("nome" <> '')
 );
 COMMENT ON COLUMN
     "tipos_pagamento"."nome" IS '`1` Dinheiro
@@ -46,39 +78,115 @@ COMMENT ON COLUMN
 
 ---*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
+CREATE TABLE "formas_pagamento_padrao"(
+    "id"    SERIAL PRIMARY KEY  NOT NULL,
+    "nome"  VARCHAR(16)         NOT NULL  CHECK ("nome" <> '')
+);
+COMMENT ON COLUMN
+    "formas_pagamento_padrao"."nome" IS '`1` Pagamentos
+    `2` Recebimentos
+    `3` Pagamentos e Recebimentos';
+
+---*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+CREATE TABLE "formas_pagamento_destino"(
+    "id"    SERIAL PRIMARY KEY  NOT NULL,
+    "nome"  VARCHAR(22)         NOT NULL  CHECK ("nome" <> '')
+);
+COMMENT ON COLUMN
+    "formas_pagamento_destino"."nome" IS '`1` Conta a receber/pagar
+    `2` Ficha financeira
+    `3` Caixa e bancos';
+
+---*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+CREATE TABLE "formas_pagamento_finalidade"(
+    "id"    SERIAL PRIMARY KEY  NOT NULL,
+    "nome"  VARCHAR(26)         NOT NULL  CHECK ("nome" <> '')
+);
+COMMENT ON COLUMN
+    "formas_pagamento_finalidade"."nome" IS '`1` Pagamentos
+    `2` Recebimentos
+    `3` Pagamentos e Recebimentos';
+
+---*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
 CREATE TABLE "formas_pagamento"(
     "id_bling"          BIGINT PRIMARY KEY  NOT NULL,
-    "nome"              VARCHAR(45)         NOT NULL,
+    "nome"              VARCHAR(45)         NOT NULL  CHECK ("nome" <> ''),
     "id_tipo_pagamento" INTEGER             NOT NULL REFERENCES "tipos_pagamento"("id"),
     "situacao"          BOOLEAN             NOT NULL,
     "fixa"              BOOLEAN             NOT NULL,
-    "padrão"            INTEGER             NOT NULL
+    "id_padrao"         INTEGER             NOT NULL REFERENCES "formas_pagamento_padrao"("id"),
+    "condicao"          VARCHAR(5)          NOT NULL  CHECK ("condicao" <> ''),
+    "id_destino"        INTEGER             NOT NULL REFERENCES "formas_pagamento_destino"("id"),
+    "id_finalidade"     INTEGER             NOT NULL REFERENCES "formas_pagamento_finalidade"("id"),
+    "taxas_aliquota"    INTEGER             NOT NULL,
+    "taxas_valor"       INTEGER             NOT NULL,
+    "taxas_prazo"       INTEGER             NOT NULL,
 );
 COMMENT ON COLUMN
     "formas_pagamento"."situacao" IS '`0` Inativa
     `1` Ativa';
 COMMENT ON COLUMN
-    "formas_pagamento"."padrão" IS '`0` Não
+    "formas_pagamento"."id_padrao" IS '`0` Não
     `1` Padrão
     `2` Padrão devolução"';
 
 ---*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
-CREATE TABLE "contas_receber"(
-    "id_bling"              BIGINT PRIMARY KEY  NOT NULL,
-    "id_situacao"           INTEGER             NOT NULL REFERENCES "contas_receber_situacao"("id"),
-    "vencimento"            DATE                NOT NULL,
-    "valor"                 INTEGER             NOT NULL,
-    "id_contato"            BIGINT              NOT NULL REFERENCES "contatos"("id_bling"),
-    "id_forma_pagamento"    BIGINT              NOT NULL REFERENCES "formas_pagamento"("id_bling"),
-    "saldo"                 INTEGER             NOT NULL,
-    "data_emissao"          DATE                NOT NULL,
-    "vencimento_original"   DATE                NOT NULL,
-    "numero_documento"      VARCHAR(63)         NOT NULL,
-    "competencia"           DATE                NOT NULL,
-    "historico"             TEXT                NOT NULL,
-    "numero_banco"          VARCHAR(63)         NOT NULL,
-    "id_portador"           BIGINT              NOT NULL REFERENCES "contas_contabeis"("id")
+CREATE TABLE contas_tipo_ocorrencia (
+        "id"      SERIAL PRIMARY KEY  NOT NULL
+    ,   "nome"    VARCHAR(12)         NOT NULL  CHECK ("nome" <> '')
+);
+COMMENT ON COLUMN
+    "contas_tipo_ocorrencia"."nome" IS '`1` Única 
+`2` Parcelada 
+`3` Mensal
+`4` Bimestral
+`5` Trimestral
+`6` Semestral
+`7` Anual
+`8` Quinzenal
+`9` Semanal';
+
+---*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+CREATE TABLE "vendedores"(
+        "id_bling"                  BIGINT PRIMARY KEY  NOT NULL
+    ,   "desconto_limite"           INTEGER             NOT NULL
+    ,   "id_loja"                   INTEGER             NOT NULL
+    ,   "comissoes_desconto_maximo" INTEGER             NOT NULL
+    ,   "comissoes_aliquota"        INTEGER             NOT NULL
+    ,   "id_contato"                BIGINT              NOT NULL REFERENCES "contatos"("id_bling")
+);
+
+---*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+CREATE TABLE "contas_receitas_despesas"(
+    "id_bling"                      BIGINT PRIMARY KEY  NOT NULL,
+    "id_situacao"                   INTEGER             REFERENCES "contas_situacao"("id"),
+    "vencimento"                    DATE                NOT NULL,
+    "valor"                         INTEGER             NOT NULL,
+    "id_contato"                    BIGINT              NOT NULL REFERENCES "contatos"("id_bling"),
+    "id_forma_pagamento"            BIGINT              NOT NULL REFERENCES "formas_pagamento"("id_bling"),
+    "saldo"                         INTEGER             NOT NULL,
+    "data_emissao"                  DATE                NOT NULL DEFAULT NOW(),
+    "vencimento_original"           DATE                NOT NULL DEFAULT NOW(),
+    "numero_documento"              VARCHAR(63)         CHECK ("numero_documento" <> ''),
+    "competencia"                   DATE                NOT NULL,
+    "historico"                     TEXT                NOT NULL,
+    "numero_banco"                  VARCHAR(63),
+    "id_portador"                   BIGINT              REFERENCES "contas_contabeis"("id_bling"),
+    "id_categoria_receita_despesa"  BIGINT              NOT NULL REFERENCES "categorias_receitas_despesas"("id_bling"),
+    "id_vendedor"                   BIGINT              REFERENCES "vendedores"("id_bling"),
+    "id_bordero"                    BIGINT,
+    "id_tipo_ocorrencia"            INTEGER             NOT NULL REFERENCES "contas_tipo_ocorrencia"("id"),
+    "considerar_dias_uteis"         BOOLEAN,
+    "dia_vencimento"                DATE                DEFAULT NOW(),
+    "numero_parcelas"               INTEGER,
+    "data_limite"                   DATE                DEFAULT NOW()
+
 );
 COMMENT ON COLUMN
     "contas_receber"."saldo" IS 'É calculado subtraindo os valores dos recebimentos do valor da conta';
@@ -88,3 +196,6 @@ COMMENT ON COLUMN
     "contas_receber"."historico" IS '"Descriçao da conta para controle interno da empresa"';
 COMMENT ON COLUMN
     "contas_receber"."numero_banco" IS '"Adicionado automaticamente com o número preenchido no cadastro do banco"';
+COMMENT ON COLUMN
+    "contas_receber"."tipo_ocorrencia" IS '`1` Única `2` Parcelada `3` Mensal `4` Bimestral `5` Trimestral `6` Semestral `7` Anual `8` Quinzenal `9` Semanal';
+
