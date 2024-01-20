1d62d21026c1c733373bc7e23ee8ceba9aa8632f
vcsil <vcsilva@poil.ufrj.br>
2024-01-20 03:10:21 -0300

    :bug: fix: corrige nome

--- db_criacao/configura_banco_de_dados_rapido/criar/scripts/051-contas_contabeis.sql
+++ db_criacao/configura_banco_de_dados_rapido/criar/scripts/051-contas_contabeis.sql
@@ -1,4 +1,4 @@
 CREATE TABLE "contas_contabeis"(
     "id_bling"  BIGINT PRIMARY KEY  NOT NULL,
-    "nome"      VARCHAR(45)         NOT NULL  CHECK ("descricao" <> '')
+    "nome"      VARCHAR(45)         NOT NULL  CHECK ("nome" <> '')
 );
