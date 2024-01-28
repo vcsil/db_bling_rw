3d8a8f332c7c84f9938d097d25131c819af18a7c
vcsil <vcsilva@poil.ufrj.br>
2024-01-28 03:22:56 -0300

    :sparkles: feat: automatiza busca do diretório das funções

---
+++ db_criacao/configura_banco_de_dados_rapido/criar/connect-database.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Pedir ao usuário o nome do arquivo a ser buscado
+nome_arquivo=connect-database.sh
+
+# Diretório onde a busca será realizada
+diretorio=$(pwd)
+
+# Verificar se o diretório existe
+if [ ! -d "$diretorio" ]; then
+    echo "Diretório não encontrado: $diretorio"
+    exit 1
+fi
+
+# Realizar a busca pelo arquivo
+caminho_arquivo=$(find "$diretorio" -type f -name "$nome_arquivo" -exec dirname {} \;)
+
+# Verificar se o arquivo foi encontrado
+if [ -z "$caminho_arquivo" ]; then
+    echo "Arquivo não encontrado: $nome_arquivo"
+fi
+
+dir_functions="${caminho_arquivo}"/src/functions.sh
+dir_database="${caminho_arquivo}"/src/database.sh
+
+source "$dir_functions";
+
+DATABASE=$(cat "$dir_database");
+
+echo "Checando status do postgres..." &&
+checkPostgres &&
+
+enterPostgresCli $DATABASE;
---
+++ db_criacao/configura_banco_de_dados_rapido/criar/destroy-database.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Pedir ao usuário o nome do arquivo a ser buscado
+nome_arquivo=destroy-database.sh
+
+# Diretório onde a busca será realizada
+diretorio=$(pwd)
+
+# Verificar se o diretório existe
+if [ ! -d "$diretorio" ]; then
+    echo "Diretório não encontrado: $diretorio"
+    exit 1
+fi
+
+# Realizar a busca pelo arquivo
+caminho_arquivo=$(find "$diretorio" -type f -name "$nome_arquivo" -exec dirname {} \;)
+
+# Verificar se o arquivo foi encontrado
+if [ -z "$caminho_arquivo" ]; then
+    echo "Arquivo não encontrado: $nome_arquivo"
+fi
+
+dir_functions="${caminho_arquivo}"/src/functions.sh
+dir_database="${caminho_arquivo}"/src/database.sh
+
+source "$dir_functions";
+
+DATABASE=$(cat "$dir_database");
+
+echo "Checando status do postgres..." &&
+checkPostgres &&
+
+echo "Destruindo banco de dados..." &&
+destroyDatabase $DATABASE;
