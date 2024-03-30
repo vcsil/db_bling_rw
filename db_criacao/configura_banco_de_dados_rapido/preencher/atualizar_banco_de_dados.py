3bb3945478462dcc8c39637b38b9da88e9319e1d
vcsil <vcsilva@poli.ufrj.br>
2024-03-30 01:02:38 -0300

    :sparkles: feat: automatiza atualizações a cada 30 min

--- db_criacao/configura_banco_de_dados_rapido/preencher/atualizar_banco_de_dados.py
+++ db_criacao/configura_banco_de_dados_rapido/preencher/atualizar_banco_de_dados.py
@@ -6,8 +6,10 @@ Created on Thu Mar 21 14:14:32 2024.
 @author: vcsil
 """
 from atualizarModulos.atualizar_modulos import atualizar_modulos
+
 from logging.handlers import RotatingFileHandler
 import logging
+import time
 
 
 def main():
@@ -16,7 +18,7 @@ def main():
     log_texto += '%(module)s; - %(funcName)s;'
     log_texto += '\t%(message)s;'
 
-    # Criando o RotatingFileHandler com tamanho máximo 2MB e mantendo até 3 arq
+    # Criando o RotatingFileHandler com tamanho máximo 2MB
     handler = RotatingFileHandler('meu_log_atualizar.txt', maxBytes=2e6)
     handler.setLevel(logging.DEBUG)
     formatter = logging.Formatter(log_texto, datefmt='%d/%m/%Y %H:%M:%S,%j')
@@ -27,7 +29,14 @@ def main():
     logger.setLevel(logging.DEBUG)
     logger.addHandler(handler)
 
-    atualizar_modulos()
+    atualiza = True
+    while atualiza:
+        try:
+            atualizar_modulos()
+            time.sleep(1800) # Aguarda 30 minutos (1800 segundos)
+        except Exception as e:
+            print(e)
+            atualiza = False
 
 
 if __name__ == "__main__":
