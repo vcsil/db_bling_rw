7e32b6618b2b679276a0ccf23352c9f6fe62568c
vcsil <vcsilva@poli.ufrj.br>
2024-03-30 00:55:08 -0300

    :sparkles: feat: cria módulo para atualizar os pedidos de venda

---
+++ db_criacao/configura_banco_de_dados_rapido/preencher/atualizarModulos/atualizarPedidosVendas/atualizar_vendas.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 29 14:09:44 2024.
+
+@author: vcsil
+"""
+
+from preencherModulos.utils import (
+    db_inserir_uma_linha, api_pega_todos_id, db_pega_varios_elementos)
+
+from atualizarModulos.atualizarPedidosVendas.utils_vendas import (
+    solicita_preenche_venda)
+from atualizarModulos.utils import solicita_item_novos
+
+from datetime import date
+from tqdm import tqdm
+import logging
+
+log = logging.getLogger(__name__)
+
+# =-=-=-=-=-=-=-=-=-=-=-=-= Atualizar Tabela Contatos =-=-=-=-=-=-=-=-=-=-=-=-=
+
+
+class AtualizarVendas():
+    """Atualiza módulo de produtos."""
+
+    def __init__(self, tabelas_colunas, db):
+        self.tabelas_colunas = tabelas_colunas
+        self.db = db
+
+    def atualizar_modulos(self, tabela: str, conn, api):
+        """Atualiza a tabela modulos da database."""
+        log.info("Atualiza módulos")
+        colunas = self.tabelas_colunas[tabela][:]
+
+        PARAM = "/situacoes/modulos"
+        modulos = solicita_item_novos(
+            param=PARAM, tabela=tabela, colunas_retorno="id_bling", conn=conn,
+            api=api, db=self.db)
+
+        list_ids_modulos = []
+
+        log.info(f"Passará por {len(modulos)} módulos")
+        for modulo in tqdm(modulos, desc="Salva modulos"):
+            modulo["id_bling"] = modulo.pop("id")
+            modulo["criar_situacoes"] = modulo.pop("criarSituacoes")
+            list_ids_modulos.append(modulo["id_bling"])
+
+            log.info(f"Insere módulo {modulo['id_bling']} no banco de dados")
+            db_inserir_uma_linha(
+                tabela=tabela, colunas=colunas, valores=modulo,
+                db=self.db,  conn=conn)
+        log.info("Módulos inseridos")
+        return list_ids_modulos
+
+    def atualizar_situacoes(self, ids_modulos: list, tabela: str, conn, api):
+        """Atualiza a tabela situacoes da database."""
+        log.info("Atualiza situacoes")
+        colunas = self.tabelas_colunas[tabela][:]
+
+        # Pega os módulos novos e os já salvos
+        ids_modulos_db = db_pega_varios_elementos(
+            tabela_busca="modulos", colunas_retorno="id_bling",
+            conn=conn, db=self.db)
+        ids_modulos += [item["id_bling"] for item in ids_modulos_db]
+
+        log.info(f"Passará por {len(ids_modulos)} módulos")
+        for id_modulo in tqdm(ids_modulos, desc="Modulo"):
+            PARAM = f"/situacoes/modulos/{id_modulo}"
+            situacoes = solicita_item_novos(
+                param=PARAM, tabela=tabela, colunas_retorno="id_bling",
+                conn=conn, api=api, db=self.db)
+
+            log.info(f"Passará por {len(situacoes)} situações")
+            for situacao in situacoes:
+                situacao["id_bling"] = situacao.pop("id")
+                situacao["id_modulo"] = id_modulo
+                situacao.pop("idHerdado")
+
+                log.info(f"Insere situacao {situacao['id_bling']}")
+                db_inserir_uma_linha(
+                    tabela=tabela, colunas=colunas, valores=situacao,
+                    db=self.db,  conn=conn)
+        log.info("Situações atualizadas")
+
+    def atualizar_transporte_frete_por_conta_de(self, tabela, conn, id_trans):
+        """Atualiza a tabela produtos_formatos da database."""
+        log.info("Atualiza transporte frete por conta de")
+        colunas = self.tabelas_colunas[tabela][:]
+
+        valores = {"id": id_trans, "nome": str(id_trans)}
+
+        db_inserir_uma_linha(
+            tabela=tabela, colunas=colunas, valores=valores,
+            db=self.db, conn=conn)
+        log.info("Termina de inserir transporte frete por conta de")
+
+    def atualizar_pedidos_vendas(self, tabela: str, conn, api, fuso):
+        """Atualiza a tabela vendas da database."""
+        hoje = date.today()
+        PARAM = "/pedidos/vendas?"
+        PARAM = f"/pedidos/vendas?dataAlteracaoInicial={hoje}&"
+        ids_vendas_alter = api_pega_todos_id(api, PARAM)
+        ids_vendas_alter.sort()
+
+        ROTA = "/pedidos/vendas/"
+        for id_venda in tqdm(ids_vendas_alter, desc="Busca pedidos de vendas"):
+            log.info(f"Solicita dados da venda {id_venda} na API")
+            solicita_preenche_venda(
+                rota=ROTA+f"{id_venda}", api=api, conn=conn, db=self.db,
+                tabelas_colunas=self.tabelas_colunas, fuso=fuso)
+            conn.commit()
+
+        log.info("Fim de atualizar pedido de venda")
+
+    def atualizar_modulo_vendas(self, conn, api, fuso):
+        """Atualizar módulo de produtos."""
+        log.info("Inicio atualização vendas")
+
+        log.info("Inicio atualizar modulos")
+        modulos = self.atualizar_modulos(tabela="modulos", conn=conn, api=api)
+
+        log.info("Inicio atualizar situações")
+        self.atualizar_situacoes(tabela="situacoes", ids_modulos=modulos,
+                                 api=api, conn=conn)
+
+        log.info("Inicio atualizar vendas")
+        self.atualizar_pedidos_vendas(tabela="vendas", conn=conn, api=api,
+                                      fuso=fuso)
+
+        log.info("Fim produtos")
+
+
+if __name__ == "__main__":
+    pass
---
+++ db_criacao/configura_banco_de_dados_rapido/preencher/atualizarModulos/atualizarPedidosVendas/utils_vendas.py
@@ -0,0 +1,257 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 29 15:34:54 2024.
+
+@author: vcsil
+"""
+
+from preencherModulos.utils import (
+    possui_informacao, db_inserir_uma_linha,  manipula_dados_endereco,
+    verifica_preenche_valor, _verifica_contato, db_pega_um_elemento,
+    db_pega_varios_elementos_controi_filtro)
+
+from atualizarModulos.utils import (db_verifica_se_existe,
+                                    db_atualizar_uma_linha)
+
+from datetime import datetime
+import logging
+
+log = logging.getLogger(__name__)
+
+# =-=-=-=-=-=-=-=-=- Funções utéis para preencher produtos. =-=-=-=-=-=-=-=-=-=
+
+
+def solicita_preenche_venda(rota: str, tabelas_colunas, api, conn, db, fuso):
+    """Solicita a conta e retorna os dados da conta manipulados."""
+    venda = api.solicita_na_api(rota)['data']
+
+    _verifica_contato(venda["contato"]["id"], tabelas_colunas,
+                      api, db, conn, fuso)
+    existe_venda = db_verifica_se_existe(
+        tabela_busca="vendas", coluna_busca="id_bling", conn=conn, db=db,
+        valor_busca=venda["id"], colunas_retorno="id_bling")
+
+    venda["alterado_em"] = datetime.now(fuso) if existe_venda else None
+
+    log.info("Manipula dados de venda")
+    _modifica_insere_valores_vendas(venda, tabelas_colunas, existe_venda, conn,
+                                    db, api)
+
+    _modifica_insere_volumes(venda, existe_venda, conn, db)
+    _modifica_insere_itens_produtos(venda["itens"], venda["id"], existe_venda,
+                                    conn, db)
+    _modifica_insere_parcelas(venda, existe_venda, conn, db)
+
+
+def _modifica_insere_valores_vendas(venda: dict, tabelas_colunas, existe,
+                                    conn, db, api):
+    dataPrevista = venda["dataPrevista"]
+    numeroLoja = venda["numeroLoja"]
+    numeroPedidoCompra = venda["numeroPedidoCompra"]
+    observacoes = venda["observacoes"]
+    observacoesInternas = venda["observacoesInternas"]
+    id_categoria = venda["categoria"]["id"]
+    id_vendedor = venda["vendedor"]["id"]
+
+    transporte = venda["transporte"]
+    t_contato = transporte["contato"]["id"]
+    valores_venda = {
+        "id_bling": venda["id"],
+        "numero": venda["numero"],
+        "numero_loja": numeroLoja if numeroLoja else None,
+        "data": venda["data"],
+        "data_saida": venda["dataSaida"],
+        "data_prevista": dataPrevista if int(dataPrevista[0]) else None,
+        "id_contato": venda["contato"]["id"],
+        "id_situacao": venda["situacao"]["id"],
+        "situacao_valor": venda["situacao"]["valor"],
+        "id_loja": venda["loja"]["id"],
+        "numero_pedido_compra": (numeroPedidoCompra if numeroPedidoCompra
+                                 else None),
+        "outras_despesas": round(venda["outrasDespesas"]*100),
+        "observacoes": observacoes if observacoes else None,
+        "observacoes_internas": (observacoesInternas if observacoesInternas
+                                 else None),
+        "desconto": round(venda["desconto"]["valor"]*100),
+        "desconto_unidade": venda["desconto"]["unidade"],
+        "id_categoria": id_categoria if id_categoria else None,
+        "id_nota_fiscal": venda["notaFiscal"]["id"],
+        "total_icms": venda["tributacao"]["totalICMS"],
+        "total_ipi": venda["tributacao"]["totalIPI"],
+        "id_vendedor": id_vendedor if id_vendedor else None,
+        "transporte_id_frete_por_conta": transporte["fretePorConta"],
+        "transporte_valor_frete": round(transporte["frete"]*100),
+        "transporte_quantidade_volumes": transporte["quantidadeVolumes"],
+        "transporte_peso_bruto": round(transporte["pesoBruto"]*100),
+        "transporte_prazo_entrega": transporte["prazoEntrega"],
+        "transporte_id_contato": t_contato if bool(t_contato) else None,
+        "transporte_id_etiqueta": _modifica_insere_etiqueta(
+            transporte["etiqueta"], tabelas_colunas, existe, venda["id"], conn,
+            db),
+        "alterado_em": venda["alterado_em"]
+    }
+    log.info("Insere pedido de venda")
+
+    if existe:
+        db_atualizar_uma_linha(
+            tabela="produtos", colunas=tabelas_colunas["produtos"][:],
+            valores=valores_venda, coluna_filtro=["id_bling"],
+            valor_filtro=valores_venda["id_bling"], db=db, conn=conn)
+    else:
+        db_inserir_uma_linha(tabela="vendas", valores=valores_venda, conn=conn,
+                             colunas=list(valores_venda.keys()), db=db)
+
+
+def _modifica_insere_etiqueta(etiqueta: dict, tabelas_colunas, existe,
+                              id_venda, conn, db):
+    log.info("Insere etiqueta de transporte da venda")
+    etiqueta["nomePais"] = "Brasil"
+    if possui_informacao(etiqueta):
+        id_pais = verifica_preenche_valor(
+            tabela_busca='endereco_paises', coluna_busca='nome',
+            valor_busca=etiqueta["nomePais"], db=db, conn=conn,
+            list_colunas=tabelas_colunas["endereco_paises"])
+        endereco = manipula_dados_endereco(
+            etiqueta, id_pais, db, conn, tabelas_colunas)
+
+        # Inserir na tabela enderecos
+        valores = list(endereco.values())
+        colunas = list(endereco.keys())
+        id_endereco = verifica_preenche_valor(
+            tabela_busca="enderecos", coluna_busca=colunas, db=db,
+            valor_busca=valores, conn=conn, list_colunas=["id"]+colunas)
+
+        nome = etiqueta["nome"]
+        valores_etiqueta = {
+            "nome": nome if nome else None,
+            "id_endereco": id_endereco
+        }
+
+        if existe:
+            return db_pega_um_elemento(
+                tabela_busca="produtos", coluna_busca="id_bling",
+                valor_busca=id_venda, colunas_retorno="transporte_id_etiqueta",
+                db=db, conn=conn)
+        else:
+            return db_inserir_uma_linha(
+                tabela="transporte_etiqueta", valores=valores_etiqueta,
+                colunas=list(valores_etiqueta.keys()), db=db, conn=conn)["id"]
+
+    else:
+        return None
+
+
+def _modifica_insere_volumes(venda: dict, existe, conn, db):
+    log.info("Insere volumes da venda")
+    volumes = venda["transporte"]["volumes"]
+    if len(volumes) > 0:
+        id_venda = venda["id"]
+        for volume in volumes:
+            valores_etiqueta = {
+                "id_bling": volume["id"],
+                "id_venda": id_venda,
+                "servico": volume["servico"],
+                "codigo_rastreamento": volume["codigoRastreamento"]
+            }
+
+            if existe:
+                db_atualizar_uma_linha(
+                    tabela="transporte_volumes", coluna_filtro=["id_bling"],
+                    colunas=list(valores_etiqueta.keys()),
+                    valores=valores_etiqueta, db=db, conn=conn,
+                    valor_filtro=valores_etiqueta["id_bling"])
+            else:
+                db_inserir_uma_linha(
+                    tabela="transporte_volumes", valores=valores_etiqueta,
+                    colunas=list(valores_etiqueta.keys()), db=db, conn=conn)
+
+
+def _modifica_insere_itens_produtos(venda_itens: list, id_venda: int, existe,
+                                    conn, db):
+    log.info("Insere produtos da venda")
+
+    if len(venda_itens) > 0:
+        for item in venda_itens:
+            id_produto = item["produto"]["id"]
+            obj_item = {
+                "id_bling": item["id"],
+                "id_venda": id_venda,
+                "id_produto": id_produto if id_produto else None,
+                "desconto": round(item["desconto"]*100),
+                "valor": round(item["valor"]*100),
+                "quantidade": item["quantidade"]
+            }
+
+            if existe:
+                db_atualizar_uma_linha(
+                    tabela="vendas_itens_produtos",
+                    colunas=list(obj_item.keys()), valores=obj_item, db=db,
+                    coluna_filtro="vendas_itens_produtos", conn=conn,
+                    valor_filtro=obj_item["id_bling"])
+            else:
+                db_inserir_uma_linha(
+                    tabela="vendas_itens_produtos", db=db, conn=conn,
+                    colunas=list(obj_item.keys()), valores=obj_item)
+
+
+def _modifica_insere_parcelas(venda: dict, existe, conn, db):
+    log.info("Insere parcelas da venda")
+    venda_parcelas = venda["parcelas"]
+
+    if len(venda_parcelas) > 0:
+        contas = _busca_conta_receber(venda, conn, db)
+
+        id_venda = venda["id"]
+        for parcela in venda_parcelas:
+            valor = round(parcela["valor"]*100)
+            id_conta_receber = None
+            if len(contas) > 0:
+                for conta in contas:
+                    if conta["valor"] == valor:
+                        id_conta_receber = conta["id_bling"]
+                        contas.remove(conta)
+                        break
+                # Quando a venda é feita pelo cartão há o desconto das taxas
+                # O valor recebido vai ser diferente
+                if not id_conta_receber:
+                    id_conta_receber = contas[0]["id_bling"]
+                    contas.pop(0)
+
+            observacoes = parcela["observacoes"]
+            obj_parcela = {
+                "id_bling": parcela["id"],
+                "id_venda": id_venda,
+                "data_vencimento": parcela["dataVencimento"],
+                "valor": valor,
+                "observacoes": observacoes if observacoes else None,
+                "id_forma_pagamento": parcela["formaPagamento"]["id"],
+                "id_conta_receber": id_conta_receber
+            }
+
+            if existe:
+                db_atualizar_uma_linha(
+                    tabela="parcelas", colunas=list(obj_parcela.keys()),
+                    valores=obj_parcela, coluna_filtro=["id_bling"],
+                    valor_filtro=obj_parcela["id_bling"], db=db, conn=conn)
+            else:
+                db_inserir_uma_linha(
+                    tabela="parcelas", colunas=list(obj_parcela.keys()),
+                    valores=obj_parcela, db=db, conn=conn)
+
+
+def _busca_conta_receber(venda: dict, conn, db):
+    filtro = f"WHERE vencimento_original='{venda['data']}' "
+    filtro += f"AND id_vendedor={venda['vendedor']['id']} "
+    filtro += f"AND id_contato={venda['contato']['id']} AND historico LIKE "
+    filtro += f"'%Ref. ao pedido de venda nº {venda['numero']}%'"
+
+    colunas_retorno = ["id_bling", "valor", "id_portador"]
+
+    return db_pega_varios_elementos_controi_filtro(
+        tabela_busca="contas_receitas_despesas", filtro=filtro,
+        colunas_retorno=colunas_retorno, db=db, conn=conn)
+
+
+if __name__ == "__main__":
+    pass
