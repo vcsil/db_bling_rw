37412e8b2ff51fb59be0579d8b1cf81e4c51c6a3
vcsil <vcsilva@poli.ufrj.br>
2024-04-17 14:47:46 -0300

    :wrench: chore: adiciona linha para alteracoes no estoque

--- db_criacao/configura_banco_de_dados_rapido/criar/init.sql
+++ db_criacao/configura_banco_de_dados_rapido/criar/init.sql
@@ -318,7 +318,8 @@ CREATE TABLE "produtos_estoques"(
     "id_produto"    BIGINT              NOT NULL REFERENCES "produtos"("id_bling"),
     "id_deposito"   BIGINT              NOT NULL REFERENCES "produtos_depositos"("id_bling"),
     "saldo_fisico"  INTEGER             NOT NULL,
-    "saldo_virtual" INTEGER             NOT NULL
+    "saldo_virtual" INTEGER             NOT NULL,
+    "alterado_em"   TIMESTAMPTZ         DEFAULT current_timestamp
 );
 
 ---*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
