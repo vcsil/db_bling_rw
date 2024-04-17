f018534148e8e9de34a0ea4fa67a8e6f53683041
vcsil <vcsilva@poli.ufrj.br>
2024-04-17 14:56:50 -0300

    :recycle: refactor:  refatora modulo de atualização de contatos

--- db_criacao/configura_banco_de_dados_rapido/preencher/atualizarModulos/atualizarContatos/atualiza_contatos.py
+++ db_criacao/configura_banco_de_dados_rapido/preencher/atualizarModulos/atualizarContatos/atualiza_contatos.py
@@ -12,12 +12,12 @@ from atualizarModulos.utils import solicita_novos_ids, solicita_item_novos
 from atualizarModulos.atualizarContatos.utils_contatos import (
     _verifica_atualiza_contato)
 
-from config.constants import DB, TABELA_COLUNAS
+from config.constants import DB, TABELAS_COLUNAS
 from colorama import Back, Style
 from tqdm import tqdm
 import logging
 
-log = logging.getLogger('root')
+log = logging.getLogger("root")
 
 # =-=-=-=-=-=-=-=-=-=-=-=-= Preencher Tabela Contatos =-=-=-=-=-=-=-=-=-=-=-=-=
 
@@ -31,79 +31,73 @@ class AtualizarContatos():
     def _atualizar_contatos_situacao(self, tabela: str, sigla, conn):
         """Atualiza a tabela contatos_situacao da database."""
         log.info("Insere nova situação de contatos")
-        colunas = TABELA_COLUNAS[tabela][:]
-        colunas.remove('id')
+        colunas = TABELAS_COLUNAS[tabela][:]
+        colunas.remove("id")
 
         valor = {"nome": sigla, "sigla": sigla}
 
-        id_situacao = db_inserir_uma_linha(tabela=tabela, colunas=colunas,
-                                           valores=valor, db=DB, conn=conn)
+        id_situacao = db_inserir_uma_linha(tabela, colunas, valor, DB, conn)
 
         return id_situacao
 
     def _atualizar_contatos_tipo(self, tabela: str, sigla, conn):
         """Atualiza a tabela contatos_tipo da database."""
         log.info("Insere novo tipo de contatos")
-        colunas = TABELA_COLUNAS[tabela][:]
-        colunas.remove('id')
+        colunas = TABELAS_COLUNAS[tabela][:]
+        colunas.remove("id")
 
         valor = {"nome": sigla, "sigla": sigla}
 
-        id_tipo = db_inserir_uma_linha(tabela=tabela, colunas=colunas,
-                                       valores=valor, db=DB, conn=conn)
+        id_tipo = db_inserir_uma_linha(tabela, colunas, valor, DB, conn)
         return id_tipo
 
     def _atualizar_contatos_indicador_inscricao_estadual(self, tabela: str,
                                                          id_iie, conn):
         """Atualiza a tabela contatos_indicador_inscricao_estadual do db."""
         log.info("Insere novo indicador inscricao ie de contatos")
-        colunas = TABELA_COLUNAS[tabela][:]
+        colunas = TABELAS_COLUNAS[tabela][:]
 
         valor = {"id": id_iie, "nome": str(id_iie)}
 
-        return db_inserir_uma_linha(tabela=tabela, colunas=colunas,
-                                    valores=valor, db=DB, conn=conn)
+        return db_inserir_uma_linha(tabela, colunas, valor, DB, conn)
 
     def atualizar_contatos_classificacao(self, conn):
         """Atualiza a tabela contatos_classificacao da database."""
         log.info("Insere nova classificacao de contatos na API")
-        tabela = 'contatos_classificacao'
-        colunas = TABELA_COLUNAS[tabela][:]
+        tabela = "contatos_classificacao"
+        colunas = TABELAS_COLUNAS[tabela][:]
 
         PARAM = "/contatos/tipos"
-        valores = solicita_item_novos(param=PARAM, tabela=tabela, conn=conn,
-                                      colunas_retorno="id_bling")
+        valores = solicita_item_novos(PARAM, tabela, "id_bling", conn)
 
         if len(valores) == 0:
             return
 
-        print(Back.GREEN + f"Insere {len(valores)} categ. novas"
-              + Style.RESET_ALL)
+        txt_fundo_verde(f"Insere {len(valores)} categ. novas")
         valores = [{"id_bling": classi["id"], "nome": classi["descricao"]}
                    for classi in valores]
 
-        db_inserir_varias_linhas(tabela=tabela, colunas=colunas,
-                                 valores=valores, db=DB, conn=conn)
+        db_inserir_varias_linhas(tabela, colunas, valores, DB, conn)
+        return
 
     def atualiza_contatos(self, conn):
         """Preenche a tabela contatos da database."""
         log.info("Insere novos contatos na API")
-        tabela = 'contatos'
+        tabela = "contatos"
 
-        ids_contatos_novos = solicita_novos_ids(
-            param="/contatos?criterio=3&", tabela_busca="contatos", conn=conn,
-            coluna_busca="id_bling", coluna_retorno="id_bling")
+        PARAM = "/contatos?criterio=3&"
+        ids_contatos_novos = solicita_novos_ids(PARAM, tabela, "id_bling",
+                                                conn)
 
         if len(ids_contatos_novos) == 0:
             return
 
         print(Back.GREEN + f"Adciona {len(ids_contatos_novos)} contatos novos"
-              + Style.RESET_ALL)
         t_desc = f"Adciona {len(ids_contatos_novos)} contatos novos"
-        for contato_novo in tqdm(ids_contatos_novos, desc=t_desc):
-            _verifica_atualiza_contato(id_contato=contato_novo, conn=conn,
-                                       tabelas_colunas=TABELA_COLUNAS)
+        for id_contato_novo in tqdm(ids_contatos_novos, desc=t_desc):
+            _verifica_atualiza_contato(id_contato_novo, conn)
             conn.commit()
+        return
 
     def atualizar_modulo_contatos(self, conn):
         """
--- db_criacao/configura_banco_de_dados_rapido/preencher/atualizarModulos/atualizarContatos/utils_contatos.py
+++ db_criacao/configura_banco_de_dados_rapido/preencher/atualizarModulos/atualizarContatos/utils_contatos.py
@@ -6,13 +6,13 @@ Created on Wed Mar 27 16:22:17 2024.
 @author: vcsil
 """
 from atualizarModulos.utils import db_verifica_se_existe
-from config.constants import FUSO, API, DB
+from config.constants import FUSO, API, DB, TABELAS_COLUNAS
 
 
-def _verifica_atualiza_contato(id_contato, tabelas_colunas, conn):
-    contato_exite = db_verifica_se_existe(
-        tabela_busca="contatos", coluna_busca="id_bling", db=DB,
-        valor_busca=[id_contato], colunas_retorno="id_bling", conn=conn)
+def _verifica_atualiza_contato(id_contato, conn):
+    tabela = "contatos"
+    coluna = "id_bling"
+    contato_exite = db_verifica_se_existe(tabela, coluna, [id_contato], conn)
 
     if contato_exite:
         return
@@ -20,9 +20,8 @@ def _verifica_atualiza_contato(id_contato, tabelas_colunas, conn):
         from preencherModulos.preencherContatos.preencher_contatos import (
             PreencherContatos)
 
-        PreencherContatos(tabelas_colunas, DB).preencher_contatos(
-            tabela="contatos", conn=conn, api=API, fuso=FUSO,
-            unicoContatoNovo=[id_contato])
+        PreencherContatos(TABELAS_COLUNAS, DB).preencher_contatos(
+            tabela, conn, API, FUSO, unicoContatoNovo=[id_contato])
 
         return
 
