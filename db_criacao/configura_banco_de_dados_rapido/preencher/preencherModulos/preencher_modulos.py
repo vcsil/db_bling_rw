3f3d93e7bbc1a83bc7969ef78fed155122fa5fee
vcsil <vcsilva@poli.ufrj.br>
2024-04-22 18:59:14 -0300

    :recycle: refactor: refatora módulo principal de preencher

--- db_criacao/configura_banco_de_dados_rapido/preencher/preencherModulos/preencher_modulos.py
+++ db_criacao/configura_banco_de_dados_rapido/preencher/preencherModulos/preencher_modulos.py
@@ -13,58 +13,36 @@ from preencherModulos.preencherContas.preencher_contas import (
     PreencherContas)
 from preencherModulos.preencherPedidosVendas.preencher_vendas import (
     PreencherVendas)
-from config.env_valores import EnvValores
-from config.conexao_api import ConectaAPI
-from config.conexao_db import ConectaDB
+from config.constants import DB
 
 import logging
-import pytz
 
 log = logging.getLogger('root')
 
 
 def preencher_modulos():
     """Preenche todos os módulos."""
-    fuso = pytz.timezone("America/Sao_Paulo")
-
-    log.info("Configura conexão com API")
-    env_api = EnvValores().env_api()
-    api = ConectaAPI(env_api)  # Carrega as variáveis de ambiente necessárias
-
-    log.info("Configura conexão com banco de dados")
-    env_db = EnvValores().env_db()
-    db = ConectaDB(env_db)  # Carrega as variáveis de ambiente necessárias
-
-    log.info("Obtém nome de todas tabelas")
-    tabelas_colunas = db.cria_dict_tabelas_colunas()
-
     # Inicia conexão com Banco de Dados
     log.info('Inicia preenchimento')
-    with db.conectar_ao_banco() as conn:
+    with DB.conectar_ao_banco() as conn:
 
         log.info("Começa preencher contatos.")
-        PreencherContatos(tabelas_colunas, db).preencher_modulo_contatos(conn,
-                                                                         api,
-                                                                         fuso)
+        PreencherContatos().preencher_modulo_contatos(conn)
         log.info("Comita contatos")
         conn.commit()
 
         log.info("Começa preencher produtos.")
-        PreencherProdutos(tabelas_colunas, db).preencher_modulo_produtos(conn,
-                                                                         api,
-                                                                         fuso)
+        PreencherProdutos().preencher_modulo_produtos(conn)
         log.info("Comita produtos")
         conn.commit()
 
         log.info("Começa preencher contas a receber.")
-        PreencherContas(tabelas_colunas, db).preencher_modulo_contas(conn, api,
-                                                                     fuso)
+        PreencherContas().preencher_modulo_contas(conn)
         log.info("Comita contas a receber")
         conn.commit()
 
         log.info("Começa preencher pedidos de venda.")
-        PreencherVendas(tabelas_colunas, db).preencher_modulo_vendas(conn, api,
-                                                                     fuso)
+        PreencherVendas().preencher_modulo_vendas(conn)
         log.info("Comita vendas")
         conn.commit()
         print('Foi')
