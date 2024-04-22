2fd36d90a60da9523d107b8892bb60828899eea9
vcsil <vcsilva@poli.ufrj.br>
2024-04-22 19:02:26 -0300

    :recycle: refactor: refatora módulo de preencher banco de dados

--- db_criacao/configura_banco_de_dados_rapido/preencher/preencher_banco_de_dados.py
+++ db_criacao/configura_banco_de_dados_rapido/preencher/preencher_banco_de_dados.py
@@ -6,30 +6,27 @@ preencher_banco_de_dados.
 Script para pegar os dados do Bling e passar para o banco de dados
 """
 from preencherModulos.preencher_modulos import preencher_modulos
+from config.constants import FUSO
 import atualizar_banco_de_dados
 
 from logging.handlers import RotatingFileHandler
 from datetime import datetime
-from pytz import timezone
-from tqdm import tqdm
 import logging
-import time
 
 # =-=-=-=-=-=-=-=-=-=-=-=-= Preencher Modulo Contatos =-=-=-=-=-=-=-=-=-=-=-=-=
 
 
 def main():
     """Executa programa."""
-    log_texto = '%(asctime)s; %(levelname)s;\t%(name)s;\t'
-    log_texto += '%(module)s; - %(funcName)s;'
-    log_texto += '\t%(message)s;'
+    log_texto = "%(asctime)s; %(levelname)s;\t%(name)s;\t"
+    log_texto += "%(module)s; - %(funcName)s;"
+    log_texto += "\t%(message)s;"
 
-    formatter = logging.Formatter(log_texto, datefmt='%d/%m/%Y %H:%M:%S,%j')
+    formatter = logging.Formatter(log_texto, datefmt="%d/%m/%Y %H:%M:%S,%j")
     # Configura o fuso horário
-    formatter.converter = lambda *args: datetime.now(
-        tz=timezone('America/Sao_Paulo')).timetuple()
+    formatter.converter = lambda *args: datetime.now(tz=FUSO).timetuple()
     # Criando o RotatingFileHandler com tamanho máximo 2MB
-    handler = RotatingFileHandler('meu_log_preencher.txt', backupCount=2,
+    handler = RotatingFileHandler("meu_log_preencher.txt", backupCount=2,
                                   maxBytes=2*1024*1024)
     handler.setFormatter(formatter)
     handler.setLevel(logging.DEBUG)
@@ -39,10 +36,6 @@ def main():
     logger.setLevel(logging.DEBUG)
     logger.addHandler(handler)
 
-    for sec in tqdm(range(99), desc="Esperando configurações do postgres"):
-        time.sleep(1)
-        print(sec)
-
     preencher_modulos()
 
     atualizar_banco_de_dados.main()
@@ -50,5 +43,5 @@ def main():
 
 # Criar uma função para cada atribulo dos dicionários
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
