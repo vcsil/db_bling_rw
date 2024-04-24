efe70bf19e83453177c4dcaacb95dd70049203a8
vcsil <vcsilva@poli.ufrj.br>
2024-04-24 19:01:38 -0300

    :wrench: chore: colore o texto da próxima atualizacao

--- db_criacao/configura_banco_de_dados_rapido/preencher/temporizador_funcao.py
+++ db_criacao/configura_banco_de_dados_rapido/preencher/temporizador_funcao.py
@@ -5,11 +5,11 @@ Created on Sat Mar 30 13:58:54 2024.
 
 @author: vcsil
 """
+from atualizarModulos.utils import txt_amarelo
 from config.constants import FUSO
 
 from datetime import datetime, timedelta
 import logging
-import pytz
 import time
 
 log = logging.getLogger('root')
@@ -33,30 +33,30 @@ def agendar_tarefa():
     if current_day in range(0, 5):  # Segunda a Sexta
         if 8 <= current_hour < 18:  # 8h as 18h - 15min
             t = f"Próxima atualização às: {agora + timedelta(minutes=15)}"
-            print(t)
+            txt_amarelo(t)
             log.info(t)
             time.sleep(60*15)
         else:
             t = f"Próxima atualização às: {agora + timedelta(minutes=45)}"
-            print(t)
+            txt_amarelo(t)
             log.info(t)
             time.sleep(60*45)
 
     elif current_day == 5:          # Sábado
         if 8 <= current_hour < 14:  # 8h as 14h - 15min
             t = f"Próxima atualização às: {agora + timedelta(minutes=15)}"
-            print(t)
+            txt_amarelo(t)
             log.info(t)
             time.sleep(60*15)
         else:
             t = f"Próxima atualização às: {agora + timedelta(hours=4)}"
-            print(t)
+            txt_amarelo(t)
             log.info(t)
             time.sleep(60*60*4)
 
     else:                           # Domingo (e outros dias)
         t = f"Próxima atualização às: {agora + timedelta(hours=4)}"
-        print(t)
+        txt_amarelo(t)
         log.info(t)
         time.sleep(60*60*4)         # 4/4h
 
