2fc8ab46887fe526317c64c069905e1d5c9a7946
vcsil <vcsilva@poli.ufrj.br>
2024-07-23 20:59:24 -0300

    :wrench: chore: tabela para lidar com saldos no balanço

--- db_criacao/05 - configuracoes/drawSQL-pgsql-export-2024-04-01.sql
+++ db_criacao/05 - configuracoes/drawSQL-pgsql-export-2024-04-01.sql
@@ -1,4 +1,14 @@
 CREATE TABLE "atualizacoes_modulos"(
-      "id"       SERIAL       PRIMARY KEY NOT NULL
-    , "datetime" TIMESTAMPTZ              NOT NULL DEFAULT current_timestamp
+        "id"       SERIAL       PRIMARY KEY NOT NULL
+    ,   "datetime" TIMESTAMPTZ              NOT NULL DEFAULT current_timestamp
+);
+
+---*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+CREATE TABLE "contagem_estoque"(
+        "id"                SERIAL       PRIMARY KEY NOT NULL
+    ,   "id_produto"        BIGINT                   NOT NULL UNIQUE REFERENCES "produtos"("id_bling")
+    ,   "codigo"            VARCHAR(120)             NOT NULL
+    ,   "quantidade_lida"   INTEGER                  NOT NULL
+    ,   "datetime"          TIMESTAMPTZ              NOT NULL DEFAULT current_timestamp
 );
