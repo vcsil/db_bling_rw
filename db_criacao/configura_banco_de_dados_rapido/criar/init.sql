40d219e936fe52053be697e2f5890e198042d7cc
vcsil <vcsilva@poli.ufrj.br>
2024-07-23 20:57:35 -0300

    :wrench: chore: tabela para lidar com saldos no balanço

--- db_criacao/configura_banco_de_dados_rapido/criar/init.sql
+++ db_criacao/configura_banco_de_dados_rapido/criar/init.sql
@@ -744,3 +744,12 @@ CREATE TABLE "atualizacoes_modulos"(
 
 ---*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
+CREATE TABLE "contagem_estoque"(
+        "id"                SERIAL       PRIMARY KEY NOT NULL
+    ,   "id_produto"        BIGINT                   NOT NULL UNIQUE REFERENCES "produtos"("id_bling")
+    ,   "codigo"            VARCHAR(120)             NOT NULL
+    ,   "quantidade_lida"   INTEGER                  NOT NULL
+    ,   "datetime"          TIMESTAMPTZ              NOT NULL DEFAULT current_timestamp
+);
+
+---*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
