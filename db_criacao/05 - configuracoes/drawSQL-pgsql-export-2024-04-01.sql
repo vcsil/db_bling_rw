ab36c28c275c92ba3fd8d83de00df095b196a01e
vcsil <vcsilva@poli.ufrj.br>
2024-11-12 11:35:37 -0300

    :wrench: chore: cria tabela para controle de estoque

--- db_criacao/05 - configuracoes/drawSQL-pgsql-export-2024-04-01.sql
+++ db_criacao/05 - configuracoes/drawSQL-pgsql-export-2024-04-01.sql
@@ -12,3 +12,28 @@ CREATE TABLE "contagem_estoque"(
     ,   "quantidade_lida"   INTEGER                  NOT NULL
     ,   "datetime"          TIMESTAMPTZ              NOT NULL DEFAULT current_timestamp
 );
+
+---*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+CREATE TABLE "registros_de_estoque"(
+        "id_bling"      SERIAL       PRIMARY KEY NOT NULL
+    ,   "id_produto"    BIGINT                   NOT NULL REFERENCES "produtos"("id_bling")
+    ,   "id_deposito"   BIGINT                   NOT NULL REFERENCES "produtos_depositos"("id_bling")
+    ,   "operacao"      CHAR(1)                  NOT NULL
+    ,   "quantidade"    INTEGER                  NOT NULL
+    ,   "preco"         INTEGER                  NOT NULL
+    ,   "custo"         INTEGER                  NOT NULL
+    ,   "observacoes"   VARCHAR(100)             NOT NULL
+    ,   "datetime"      TIMESTAMPTZ              NOT NULL DEFAULT current_timestamp
+);
+
+---*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+CREATE TABLE "registros_de_comparacao_balanco"(
+        "id"            SERIAL       PRIMARY KEY NOT NULL
+    ,   "id_produto"    BIGINT                   NOT NULL REFERENCES "produtos"("id_bling")
+    ,   "id_deposito"   BIGINT                   NOT NULL REFERENCES "produtos_depositos"("id_bling")
+    ,   "saldo_antes"   INTEGER                  NOT NULL
+    ,   "saldo_depois"  INTEGER                  NOT NULL
+    ,   "date"          DATE                     NOT NULL DEFAULT NOW()
+);
