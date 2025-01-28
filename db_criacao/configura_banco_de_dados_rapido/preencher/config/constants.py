c74a323e13c92eb24d6f3f715a2eda561c472a4d
vcsil <vcsilva@poli.ufrj.br>
2025-01-28 16:20:21 -0300

    :wrench: chore: sinaliza fim da espera pelo postgresql

--- db_criacao/configura_banco_de_dados_rapido/preencher/config/constants.py
+++ db_criacao/configura_banco_de_dados_rapido/preencher/config/constants.py
@@ -19,6 +19,7 @@ log = logging.getLogger("root")
 for sec in tqdm(range(99), desc="Esperando configurações do postgres",
                 position=1):
     time.sleep(1)
+print("Passou")
 
 FUSO = pytz.timezone("America/Sao_Paulo")
 
